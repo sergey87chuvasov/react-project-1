1554ac4ed4695fb6569cde90d457f232c61c0c69
Sergey87 <serge.techno@gmail.com>
2022-04-23 01:59:57 +0300

    a little bit about useState

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,9 +1,9 @@
 import './Card.css';
 
-function Card(props) {
+const Card = (props) => {
   // что бы корректно оторажались стили в <Card className='cost-item'>.. тк из-да Card => className теперь не работает, то есть решение СОЗД переменную и укажем в ней все классы которые мы хотим употреблять
   const classes = 'card ' + props.className;
   return <div className={classes}>{props.children}</div>;
-}
+};
 
 export default Card;
