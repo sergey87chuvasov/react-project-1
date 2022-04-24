3585fc6950a51f45d295368e3f0e8eb96ff5428f
Sergey87 <serge.techno@gmail.com>
2022-04-24 18:49:20 +0300

    add two-way binding

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -1,20 +1,94 @@
+import React, { useState } from 'react';
 import './CostForm.css';
 
 const CostForm = () => {
+  // пустая строка тк в начале отображения инпутов ничего нет, деструктур для получения 2х эл
+  const [inputName, setInputName] = useState('');
+  const [inputAmount, setInputAmount] = useState('');
+  const [inputDate, setInputDate] = useState('');
+
+  // общий подход
+  // const [userInput, setUserInput] = useState({
+  //   name: '',
+  //   amount: '',
+  //   date: '',
+  // });
+
+  const nameChangeHandler = (event) => {
+    setInputName(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   name: event.target.value,
+    // });
+    // setUserInput((previousState) => {
+    //   return {
+    //     ...previousState,
+    //     name: event.target.value,
+    //   };
+    // });
+  };
+
+  const amountChangeHandler = (event) => {
+    setInputAmount(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   amount: event.target.value,
+    // });
+  };
+
+  const dateChangeHandler = (event) => {
+    setInputDate(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   date: event.target.value,
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const costData = {
+      name: inputName,
+      amount: inputAmount,
+      date: new Date(inputDate),
+    };
+
+    console.log(costData);
+    // запись нового значения в инпут  + value={inputName} те очистка инпутов
+    setInputName('');
+    setInputAmount('');
+    setInputDate('');
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-cost__controls'>
         <div className='new-cost__control'>
           <label>Название</label>
-          <input type='text' />
+          <input type='text' value={inputName} onChange={nameChangeHandler} />
         </div>
         <div className='new-cost__control'>
           <label>Сумма</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input
+            type='number'
+            onChange={amountChangeHandler}
+            value={inputAmount}
+            min='0.01'
+            step='0.01'
+          />
         </div>
         <div className='new-cost__control'>
           <label>Дата</label>
-          <input type='date' min='2019-01-01' step='2022-12-31' />
+          <input
+            type='date'
+            value={inputDate}
+            onChange={dateChangeHandler}
+            min='2019-01-01'
+            step='2022-12-31'
+          />
         </div>
         <div className='new-cost__actions'>
           <button type='submit'>Добавить расход</button>
